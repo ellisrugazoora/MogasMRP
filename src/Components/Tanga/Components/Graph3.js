6d40aaf01c22f94eaf91810f6b9eb1ae291cff47
ellisrugazoora <ellisrgrz@gmail.com>
2024-01-27 01:15:14 -0800

    updated formulas

--- src/Components/Tanga/Components/Graph3.js
+++ src/Components/Tanga/Components/Graph3.js
@@ -22,9 +22,9 @@ function Graph3(props){
         bo1: 1500, bo2: 900, bo3:600, bo4: 200, bo5: 200, bo6: 200, ad1:200, ad2:200, ad3:200, ad4:200, ad5: 200, ad6: 200, ad7: 200, ad8: 200, ad9:200, ad10: 200, ad11: 200, ad12: 200, //Starting inventory is a prop
         formulas: {
             _2T: populateformula({bo1:0.9050, bo5:0.08, ad9:0.0150}),
-            _4T: populateformula({bo1:0.8370,bo3:0.0530,ad2:0.002, ad5:0.0500, ad7:0.0580}),
+            _4T: populateformula({bo1:0.877,bo3:0,ad2:0.002, ad5:0.063, ad7:0.0550}),
             atfIII: populateformula({bo2:0.6097,bo4:0.3,ad8:0.09,ad11:0.0003}),
-            DuramaxHD: populateformula({bo1:0.7780, bo3:0.1720,ad1:0.006,ad6:0.0440}), 
+            DuramaxHD: populateformula({bo1:0.733, bo3:0.23,ad1:0.003,ad6:0.034}), 
             FrontiaX: populateformula({bo1:0.6238, bo2:0.2442, ad2:0.002,ad5:0.0760,ad7:0.0540}),
             Geo80W90:populateformula({bo1:0.7600,bo3:0.2050,ad2:0.015,ad4:0.02}),
             Geo85W140:populateformula({bo1:0.030,bo3:0.927,ad2:0.003,ad4:0.0400}),
@@ -57,6 +57,9 @@ function Graph3(props){
     title: {
         text: `Mogas LOBP Production projection ${props.title}`,
     },
+    subtitle: {
+        text: "In thousands of Litres",
+      },
     data: [ //each object is a group (stacked or not)
         data(args).bo1, data(args).bo2, data(args).bo3, data(args).bo4,data(args).bo5, data(args).ad1,data(args).ad2,data(args).ad3, 
         data(args).ad4, data(args).ad5, data(args).ad6, data(args).ad7, data(args).ad8, data(args).ad9,
@@ -167,26 +170,26 @@ function Graph3(props){
         prod16: {col1: "Turbofleet Sae 15W", col2: <NumberInp value={args.TurbofleetSae15W} prod="TurbofleetSae15W" onChange={prodtable} init={args.TurbofleetSae15W} />, col4: <Button id='max' name='TurbofleetSae15W' onClick={tablebutton}>Set Max</Button>},
         total: {col1: "Total", col2: product_total(args), col3: <Button isDisabled>Empty</Button> }
     }
-    var inv_table_columns = ["Inventory", "Required", "In stock","In Transit", "Deficit/surplus"];
+    var inv_table_columns = ["Inventory", "Required", "In stock","Lead time (days)", "Next order qty"/*,  "Order date", "Order amount"*/];
     var inv_table_data = {
-        inv1: {col1: "500SN/600N", col2: data(args).bo1.sum.toFixed(2), col3: <NumberInp prod="bo1" init={args.bo1} onChange={inv_table} value={args.bo1} />, col4: <NumberInp prod="bo1" init={0} onChange={transit}/>, col5: (args.bo1 - data(args).bo1.sum).toFixed(2)},
-        inv2: {col1: "150SN", col2: data(args).bo2.sum.toFixed(2), col3: <NumberInp prod="bo2" init={args.bo2} onChange={inv_table} value={args.bo2} />, col4: <NumberInp prod="bo2" init={0} onChange={transit}/>,col5: (args.bo2 - data(args).bo2.sum).toFixed(2)},
-        inv4: {col1: "BS150", col2: data(args).bo3.sum.toFixed(2), col3: <NumberInp prod="bo3" init={args.bo3} onChange={inv_table} value={args.bo3} />, col4: <NumberInp prod="bo3" init={0} onChange={transit}/>,col5: (args.bo3 - data(args).bo3.sum).toFixed(2)},
-        inv6: {col1: "SN80/SN100", col2: data(args).bo4.sum.toFixed(2), col3: <NumberInp prod="bo4" init={args.bo4} onChange={inv_table} value={args.bo4} />, col4: <NumberInp prod="bo4" init={0} onChange={transit}/>,col5: (args.bo4 - data(args).bo4.sum).toFixed(2)},
-        inv5: {col1: "DPK", col2: data(args).bo5.sum.toFixed(2), col3: <NumberInp prod="bo5" init={args.bo5} onChange={inv_table} value={args.bo5} />, col4: <NumberInp prod="bo5" init={0} onChange={transit}/>,col5: (args.bo5 - data(args).bo5.sum).toFixed(2)},
+        inv1: {col1: "500SN/600N", col2: data(args).bo1.sum.toFixed(2), col3: <NumberInp prod="bo1" init={args.bo1} onChange={inv_table} value={args.bo1} />, col4: 60, col5: (args.bo1 - data(args).bo1.sum).toFixed(2)},
+        inv2: {col1: "150SN", col2: data(args).bo2.sum.toFixed(2), col3: <NumberInp prod="bo2" init={args.bo2} onChange={inv_table} value={args.bo2} />, col4: 60,col5: (args.bo2 - data(args).bo2.sum).toFixed(2)},
+        inv3: {col1: "BS150", col2: data(args).bo3.sum.toFixed(2), col3: <NumberInp prod="bo3" init={args.bo3} onChange={inv_table} value={args.bo3} />, col4: 60,col5: (args.bo3 - data(args).bo3.sum).toFixed(2)},
+        inv4: {col1: "SN80/SN100", col2: data(args).bo4.sum.toFixed(2), col3: <NumberInp prod="bo4" init={args.bo4} onChange={inv_table} value={args.bo4} />, col4: 60,col5: (args.bo4 - data(args).bo4.sum).toFixed(2)},
+        inv5: {col1: "DPK", col2: data(args).bo5.sum.toFixed(2), col3: <NumberInp prod="bo5" init={args.bo5} onChange={inv_table} value={args.bo5} />, col4: 60,col5: (args.bo5 - data(args).bo5.sum).toFixed(2)},
         
-        inv6: {col1: "TBN+", col2: data(args).ad1.sum.toFixed(2), col3: <NumberInp prod="ad1" init={args.ad1} onChange={inv_table} value={args.ad1} />, col4: <NumberInp prod="ad1" init={0} onChange={transit}/>,col5: (args.ad1 - data(args).ad1.sum).toFixed(2)},
-        inv7: {col1: "PPD", col2: data(args).ad2.sum.toFixed(2), col3: <NumberInp prod="ad2" init={args.ad2} onChange={inv_table} value={args.ad2} />, col4: <NumberInp prod="ad2" init={0} onChange={transit}/>,col5: (args.ad2 - data(args).ad2.sum).toFixed(2)},
-        inv8: {col1: "CI-4", col2: data(args).ad3.sum.toFixed(2), col3: <NumberInp prod="ad3" init={args.ad3} onChange={inv_table} value={args.ad3} />, col4: <NumberInp prod="ad3" init={0} onChange={transit}/>, col5: (args.ad3 - data(args).ad3.sum).toFixed(2)},
-        inv9: {col1: "BS200", col2: data(args).ad4.sum.toFixed(2), col3: <NumberInp prod="ad4" init={args.ad4} onChange={inv_table} value={args.ad4} />, col4: <NumberInp prod="ad4" init={0} onChange={transit}/>,col5: (args.ad4 - data(args).ad4.sum).toFixed(2)},
-        inv10: {col1: "VII", col2: data(args).ad5.sum.toFixed(2), col3: <NumberInp prod="ad5" init={args.ad5} onChange={inv_table} value={args.ad5} />, col4: <NumberInp prod="ad5" init={0} onChange={transit}/>,col5: (args.ad5 - data(args).ad5.sum).toFixed(2)},
-        inv11: {col1: "MONO PA EO", col2: data(args).ad6.sum.toFixed(2), col3: <NumberInp prod="ad6" init={args.ad6} onChange={inv_table} value={args.ad6} />, col4: <NumberInp prod="ad6" init={0} onChange={transit}/>,col5: (args.ad6 - data(args).ad6.sum).toFixed(2)},
-        inv12: {col1: "4T PA PEO", col2: data(args).ad7.sum.toFixed(2), col3: <NumberInp prod="ad7" init={args.ad7} onChange={inv_table} value={args.ad7} />, col4: <NumberInp prod="ad7" init={0} onChange={transit}/>,col5: (args.ad7 - data(args).ad7.sum).toFixed(2)},
-        inv13:{col1: "ATF PA", col2: data(args).ad8.sum.toFixed(2), col3: <NumberInp prod="ad8" init={args.ad8} onChange={inv_table} value={args.ad8} />, col4: <NumberInp prod="ad8" init={0} onChange={transit}/>,col5: (args.ad8 - data(args).ad8.sum).toFixed(2)},
-        inv14:{col1: "2T PA", col2: data(args).ad9.sum.toFixed(2), col3: <NumberInp prod="ad9" init={args.ad9} onChange={inv_table} value={args.ad9} />, col4: <NumberInp prod="ad9" init={0} onChange={transit}/>,col5: (args.ad9 - data(args).ad9.sum).toFixed(2)},
-        inv15:{col1: "HYA", col2: data(args).ad10.sum.toFixed(2), col3: <NumberInp prod="ad10" init={args.ad10} onChange={inv_table} value={args.ad10} />, col4: <NumberInp prod="ad10" init={0} onChange={transit}/>,col5: (args.ad10 - data(args).ad10.sum).toFixed(2)},
-        inv16:{col1: "DYE", col2: data(args).ad11.sum.toFixed(2), col3: <NumberInp prod="ad11" init={args.ad11} onChange={inv_table} value={args.ad11} />, col4: <NumberInp prod="ad11" init={0} onChange={transit}/>,col5: (args.ad11 - data(args).ad11.sum).toFixed(2)},
-        inv17:{col1: "TURB", col2: data(args).ad12.sum.toFixed(2), col3: <NumberInp prod="ad12" init={args.ad12} onChange={inv_table} value={args.ad12} />, col4: <NumberInp prod="ad12" init={0} onChange={transit}/>,col5: (args.ad12 - data(args).ad12.sum).toFixed(2)}
+        inv6: {col1: "TBN+", col2: data(args).ad1.sum.toFixed(2), col3: <NumberInp prod="ad1" init={args.ad1} onChange={inv_table} value={args.ad1} />, col4: 40,col5: (args.ad1 - data(args).ad1.sum).toFixed(2)},
+        inv7: {col1: "PPD", col2: data(args).ad2.sum.toFixed(2), col3: <NumberInp prod="ad2" init={args.ad2} onChange={inv_table} value={args.ad2} />, col4: 40,col5: (args.ad2 - data(args).ad2.sum).toFixed(2)},
+        inv8: {col1: "CI-4", col2: data(args).ad3.sum.toFixed(2), col3: <NumberInp prod="ad3" init={args.ad3} onChange={inv_table} value={args.ad3} />, col4: 40, col5: (args.ad3 - data(args).ad3.sum).toFixed(2)},
+        inv9: {col1: "BS200", col2: data(args).ad4.sum.toFixed(2), col3: <NumberInp prod="ad4" init={args.ad4} onChange={inv_table} value={args.ad4} />, col4: 40,col5: (args.ad4 - data(args).ad4.sum).toFixed(2)},
+        inv10: {col1: "VII", col2: data(args).ad5.sum.toFixed(2), col3: <NumberInp prod="ad5" init={args.ad5} onChange={inv_table} value={args.ad5} />, col4: 40,col5: (args.ad5 - data(args).ad5.sum).toFixed(2)},
+        inv11: {col1: "MONO PA EO", col2: data(args).ad6.sum.toFixed(2), col3: <NumberInp prod="ad6" init={args.ad6} onChange={inv_table} value={args.ad6} />, col4: 40,col5: (args.ad6 - data(args).ad6.sum).toFixed(2)},
+        inv12: {col1: "4T PA PEO", col2: data(args).ad7.sum.toFixed(2), col3: <NumberInp prod="ad7" init={args.ad7} onChange={inv_table} value={args.ad7} />, col4: 40,col5: (args.ad7 - data(args).ad7.sum).toFixed(2)},
+        inv13:{col1: "ATF PA", col2: data(args).ad8.sum.toFixed(2), col3: <NumberInp prod="ad8" init={args.ad8} onChange={inv_table} value={args.ad8} />, col4: 40,col5: (args.ad8 - data(args).ad8.sum).toFixed(2)},
+        inv14:{col1: "2T PA", col2: data(args).ad9.sum.toFixed(2), col3: <NumberInp prod="ad9" init={args.ad9} onChange={inv_table} value={args.ad9} />, col4: 40,col5: (args.ad9 - data(args).ad9.sum).toFixed(2)},
+        inv15:{col1: "HYA", col2: data(args).ad10.sum.toFixed(2), col3: <NumberInp prod="ad10" init={args.ad10} onChange={inv_table} value={args.ad10} />, col4: 40,col5: (args.ad10 - data(args).ad10.sum).toFixed(2)},
+        inv16:{col1: "DYE", col2: data(args).ad11.sum.toFixed(2), col3: <NumberInp prod="ad11" init={args.ad11} onChange={inv_table} value={args.ad11} />, col4: 40,col5: (args.ad11 - data(args).ad11.sum).toFixed(2)},
+        inv17:{col1: "TURB", col2: data(args).ad12.sum.toFixed(2), col3: <NumberInp prod="ad12" init={args.ad12} onChange={inv_table} value={args.ad12} />, col4: 40,col5: (args.ad12 - data(args).ad12.sum).toFixed(2)}
     };//MONO PA EO; 4T PA PEO; ATF PA; 2T PA; HYA; DYE; TURB; TM PA
     return (
         <div>
